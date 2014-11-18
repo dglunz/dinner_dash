2ef3fe133a8b5660baf90dda1b9e147e8740fa96
David Bayendor <dbayendor@gmail.com>
2014-11-17 20:37:15 -0700

    Add validations to user model
    * name
    
    * email
    
    * display_name length
    
    * password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+
+  validates :name, presence: true
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+  validates :email, presence: true,
+                    format: { with: EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :display_name, length: { in: 2..32 }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+  let(:user) do
+    User.new(name: 'User', email: 'user@user.com', display_name: 'user',
+             password: 'password', password_confirmation: 'password')
+  end
+
+  it 'is valid' do
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    user.name = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid if optional display name is too short' do
+    user.display_name = 'A'
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid if optional display name is too long' do
+    user.display_name = ('A' * 33)
+    expect(user).to_not be_valid
+  end
+  it 'is invalid without an email address' do
+    user.email = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid without a valid email address' do
+    addresses = %w(user@foo,com user_at_foo.org example.user@foo.
+                   foo@bar_baz.com foo@bar_baz.com)
+    addresses.each do |invalid_address|
+      user.email = invalid_address
+      expect(user).to_not be_valid
+    end
+  end
+
+  it 'is invalid if the email is not unique' do
+    user.save
+    invalid_user = user.dup
+    invalid_user.email = user.email.upcase
+    expect(invalid_user.save).to be false
+  end
+
+  it 'is invalid if password is not present' do
+    user.password = user.password_confirmation = ''
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid if password is too short' do
+    user.password = user.password_confirmation = 'a' * 5
+    expect(user).to_not be_valid
+  end
+  it 'is invalid if password does not match confirmation' do
+    user.password = 'foobar'
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid if password confirmation is nil' do
+    user.password_confirmation = nil
+    expect(user).to_not be_valid
+  end
 end
