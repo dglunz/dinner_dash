79a6b3d801cee097d11703495820e9f5a9caa126
Daniel Glunz <dannyglunz@gmail.com>
2014-11-18 13:24:26 -0700

    add more tests/implement data validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ActiveRecord::Base
-  validates :title, :description, :price, presence: true
+  validates :title, uniqueness: true, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }, format: { with: /\d+(\.\d{1,2})?/ }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Item, :type => :model do
   let(:item) { Item.new(valid_attributes) }
   let(:valid_attributes) { {
     title: 'coffee',
-    description: 'legal stimulant',
+    description: 'legal stimulants for cheap',
     price: 2.99
   } }
 
@@ -27,5 +27,39 @@ RSpec.describe Item, :type => :model do
     expect(item).to be_invalid
   end
 
+  xit 'belongs to at least one category' do
+  end
+
+  it 'is not valid if title is an empty' do
+    item.title = ""
+    expect(item).to be_invalid
+  end
+
+  it 'is not valid if description is empty' do
+    item.description = ""
+    expect(item).to be_invalid
+  end
+
+  it 'must have a unique title' do
+    item.save
+    item2 = Item.create(valid_attributes)
+    expect(item2).to be_invalid
+
+    item2.title = 'espresso'
+    expect(item2).to be_valid
+  end
+
+  it 'is invalid if the price is less than zero' do
+    item.price = -1.00
+    expect(item).to be_invalid
+  end
+
+  it 'is invalid if price is anything other than a decimal numeric value' do
+    item.price = "alkdj"
+    expect(item).to be_invalid
+  end
+
+  xit 'has a default photo if empty' do
+  end
 
 end
