dca6202f72d14e9d89b149335f7ad0958c8ef403
Daniel Glunz <dannyglunz@gmail.com>
2014-11-18 14:27:50 -0700

    implement category model and category/item relationship

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
+  has_and_belongs_to_many :categories
   validates :title, uniqueness: true, presence: true
-  validates :description, presence: true
+  validates :description, :categories, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }, format: { with: /\d+(\.\d{1,2})?/ }
 end
