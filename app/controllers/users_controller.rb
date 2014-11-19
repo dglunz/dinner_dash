b76909495daf373b5038f0837ff44a992d509a89
David Bayendor <dbayendor@gmail.com>
2014-11-18 20:21:20 -0700

    Add feature to test login / logout path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user, notice: 'User successfully created.'
+      redirect_to login_path, notice: "User created. Please log in."
     else
+      flash.now[:notice] = "User could not be created."
       render :new
     end
   end
@@ -23,6 +24,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :display_name)
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation, :display_name)
   end
 end
--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -13,8 +13,37 @@ describe 'the application', type: :feature do
       expect(page).to have_link('Login', href: login_path)
     end
 
-    it 'does not link to logout' do
+    it 'has a sign up link' do
+      expect(page).to have_link('Sign Up', href: new_user_path)
+    end
+
+    it 'does not have a logout link' do
       expect(page).not_to have_link('Logout', href: logout_path)
     end
   end
+
+  context 'when logged in' do
+
+    xit 'creates and logs in a new user' do
+      visit root_path
+      click_link 'Sign Up'
+      fill_in "Name", :with => "Name"
+      fill_in "Email", :with => "email@email.com"
+      fill_in "Password", :with => "password"
+      fill_in "Password confirmation", :with => "password"
+      click_button 'Submit'
+      expect(page).to have_content('User created.')
+
+      click_link 'Login'
+      expect(page).to have_link('Login', href: login_path)
+      fill_in "Email", :with => "email@email.com"
+      fill_in "Password", :with => "password"
+      expect(page).to have_link('Logout', href: logout_path)
+    end
+
+    xit 'does not have a login link' do
+      expect(page).not_to have_link('Login', href: login_path)
+    end
+  end
 end
+
