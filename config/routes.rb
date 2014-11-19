851ab7b209b2ff46304ca71ecf6f2619ffda55d8
Daniel Glunz <dannyglunz@gmail.com>
2014-11-19 09:18:09 -0700

    add paperclip and item routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get 'address/edit'
 
+  resources :items
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
