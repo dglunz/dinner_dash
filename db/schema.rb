3e6a98e11d8215a8011f79ed12bc6da984172996
Adam Smith <adamsmith910@aol.com>
2014-11-20 15:46:09 -0700

    added user id to addresses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141120222904) do
+ActiveRecord::Schema.define(version: 20141120224452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141120222904) do
     t.string   "zip_code"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "addresses_orders", id: false, force: true do |t|
