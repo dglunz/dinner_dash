33021a9bb8a20aa5c99248fb2af63bfe0c0988ed
Adam Smith <adamsmith910@aol.com>
2014-11-20 12:38:05 -0700

    Order model with passing tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ActiveRecord::Base
+  belongs_to :user
+  
+  validates :delivery, presence: true
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Order, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:order) do 
+    Order.new(delivery: true)
+  end
+
+  it 'is valid' do 
+    expect(order).to be_valid
+  end
+
+  it 'is invalid without a delivery status' do 
+    order.delivery = nil
+    expect(order).to_not be_valid
+  end
+
 end
