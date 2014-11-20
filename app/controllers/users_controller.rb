9e93dd9a5f89c71d0c19c5ee4506421cccd10384
David Bayendor <dbayendor@gmail.com>
2014-11-19 21:44:20 -0700

    Add admin / user before_actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_admin, only: [:index, :update, :destroy]
+  before_action :current_user, only: [:show]
 
   def index
     @users = User.all
