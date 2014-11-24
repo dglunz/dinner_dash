24efdd9ead02af00236bda83a6f59d5d93b5214e
Adam Smith <adamsmith910@aol.com>
2014-11-24 09:14:39 -0700

    fixed error

--- db/migrate/20141121121827_create_order_items.rb
+++ db/migrate/20141121121827_create_order_items.rb
@@ -1,7 +1,7 @@
 class CreateOrderItems < ActiveRecord::Migration
   def change
     create_table :order_items do |t|
-      t.integer :order_id []
+      t.integer :order_id
       t.integer :item_id
     end
   end
