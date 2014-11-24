9447e53f98c13d077ac33a604df4f5703d9bd8ec
David Bayendor <dbayendor@gmail.com>
2014-11-23 18:49:33 -0700

    Create user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(
+  name: 'Rachel Warbelow',
+  email: 'demo+rachel@jumpstartlab.com',
+  password: 'password',
+  password_confirmation: 'password',
+  display_name: '',
+  admin: false
+  )
+
+User.create(
+  name: 'Jeff',
+  email: 'demo+jeff@jumpstartlab.com',
+  password: 'password',
+  password_confirmation: 'password',
+  display_name: 'j3',
+  admin: false
+  )
+
+User.create(
+  name: 'Jorge Tellez',
+  email: 'demo+jorge@jumpstartlab.com',
+  password: 'password',
+  password_confirmation: 'password',
+  display_name: 'novohispano',
+  admin: false
+  )
+
+User.create(
+  name: 'Josh Cheek',
+  email: 'demo+josh@jumpstartlab.com',
+  password: 'password',
+  password_confirmation: 'password',
+  display_name: 'josh',
+  admin: true
+  )
