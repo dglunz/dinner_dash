714aa00915f792882b75a9fea3941ebe0b08d6bc
David Bayendor <dbayendor@gmail.com>
2014-11-26 09:47:45 -0700

    Add address factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -18,6 +18,11 @@ FactoryGirl.define do  factory :order do
   end
 
   factory :address do
-
+    street_number { Faker::Address.building_number }
+    street_name { Faker::Address.street_name }
+    city { Faker::Address.city }
+    state { Faker::Address.state_abbr }
+    zip_code { Faker::Address.zip }
+    user_id 0
   end
 end
