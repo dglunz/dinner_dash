36d633ae5248bb1bc042b48a33d2ba435677e67f
Daniel Glunz <dannyglunz@gmail.com>
2014-12-01 10:26:59 -0700

    add item association to order on creation

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,8 @@ class Order < ActiveRecord::Base
   belongs_to :user
   belongs_to :address
 
+  has_and_belongs_to_many :items
+
   validates_inclusion_of :delivery, :in => [true, false]
   validates_inclusion_of :pending, :in => [true, false]
 
