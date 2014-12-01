7e6538c5ab107605ffe04c5bb2228101f6d0559f
Daniel Glunz <dannyglunz@gmail.com>
2014-12-01 09:12:28 -0700

    skip order controller test until admin controller

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe OrdersController, :type => :controller do
 
   let(:valid_session) { {} }
 
-  describe "GET index" do 
-    it "assigns all orders as @orders" do 
+  describe "GET index" do
+    xit "assigns all orders as @orders" do
       order = Order.create! valid_attributes
       get :index, {}, valid_session
       expect(assigns(:orders)).to eq([order])
