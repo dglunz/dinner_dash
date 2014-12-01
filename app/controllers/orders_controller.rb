197e53e32a84280bdd604c19b44fc65970a6caa8
Daniel Glunz <dannyglunz@gmail.com>
2014-11-30 18:45:01 -0700

    add create to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :current_user
+  before_action :require_admin, only: [:index]
+
   def index
     @orders = Order.all
   end
@@ -8,10 +11,33 @@ class OrdersController < ApplicationController
   end
 
   def new
-    @order = Order.new
-    @cart_items = @cart.items
+    if current_user
+      @order = Order.new
+      @cart_items = @cart.items
+    else
+      flash[:notice] = "You need to be logged in to checkout."
+      redirect_to login_path
+    end
   end
 
   def edit
+  end
+
+  def create
+    @order = Order.new(order_params)
+    binding.pry
+    @order.user = @current_user
+    @order.pending = true
+    if @order.save
+      flash[:notice] = "Order created"
+      redirect_to order_path(@order)
+    else
+      flash[:notice] = "Order could not be created. Try checking out again."
+      redirect_to cart_items_path
+    end
+  end
+
+  def order_params
+    params.require(:order).permit(:delivery, :user)
   end
 end
