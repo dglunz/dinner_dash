0b17c3f48316e24f8ed23c2715b2e45a25b38b7a
David Bayendor <dbayendor@gmail.com>
2014-12-02 09:11:01 -0700

    Add feature test for admin link and page

---
+++ spec/features/admin/admin_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+require 'capybara/rails'
+require 'capybara/rspec'
+
+describe 'the admin page', type: :feature do
+  let(:admin) { FactoryGirl.create(:admin) }
+
+  before do
+    visit root_path
+  end
+
+  context 'when logged in' do
+    before do
+      click_link 'Login'
+      fill_in 'Email', with: admin.email
+      fill_in 'Password', with: admin.password
+      click_button 'Login'
+    end
+
+    describe 'admin link' do
+      it 'should be visible' do
+        expect(page).to have_link('Admin')
+      end
+
+      it 'takes me to the admin page' do
+
+      end
+    end
+  end
+
+end
